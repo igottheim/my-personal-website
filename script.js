eb5fa2392892fd29f52dd95ad8ffb25aebd6fc5c
ian gottheim <iangottheim@gmail.com>
2022-07-23 12:15:23 -0400

    some updates

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Ian's Running Page!";
 document.querySelector("body").appendChild(h2);
+const a = document.getElementById("demo");
+
+function mouseOver() {
+  a.style.color = "red";
+}
+
+function mouseOut() {
+  a.style.color = "black";
+}
+
+a.addEventListener("mouseover", mouseOver);
+a.addEventListener("mouseout", mouseOut);
